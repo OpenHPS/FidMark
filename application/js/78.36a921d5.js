6ade869e2f53bf6bfd2ac2eaf2b7ee4173a408fa
Maximvdw <Maximvdw@users.noreply.github.com>
2023-12-08 10:39:36 +0000

    deploy: 93508bd97bb3a2d4a43822d6d7a373b75d8e25ae

--- application/js/78.36a921d5.js
+++
@@ -1,6 +0,0 @@
-"use strict";(globalThis["webpackChunkeswc2024"]=globalThis["webpackChunkeswc2024"]||[]).push([[78],{6078:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>startStatusTap});var n=s(65),o=s(8487),r=s(6587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const startStatusTap=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,a=document.elementFromPoint(t/2,s/2);if(!a)return;const i=(0,o.a)(a);i&&new Promise((e=>(0,r.c)(i,e))).then((()=>{(0,n.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,o.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.36a921d5.js.map
